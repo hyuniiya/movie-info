bf8df7957f8c176a8317e3a0c645ffbd580dac11
Juhyun Park <wngus7988.10@gmail.com>
2024-04-16 16:41:19 +0900

    init

---
+++ src/lib/dummydata.js
@@ -0,0 +1,62 @@
+export const dummyAllMovieList = [
+  {
+    id: 1,
+    title: "노량",
+    year: 2023,
+    category: "액션, 드라마",
+  },
+  {
+    id: 2,
+    title: "반지의 제왕: 왕의 귀환",
+    year: 2003,
+    category: "판타지, 모험",
+  },
+  {
+    id: 3,
+    title: "인셉션",
+    year: 2010,
+    category: "SF, 스릴러",
+  },
+  {
+    id: 4,
+    title: "라라랜드",
+    year: 2016,
+    category: "음악, 로맨스",
+  },
+  {
+    id: 5,
+    title: "택시 운전사",
+    year: 2017,
+    category: "드라마, 역사",
+  },
+  {
+    id: 6,
+    title: "어벤져스: 엔드게임",
+    year: 2019,
+    category: "액션, SF",
+  },
+  {
+    id: 7,
+    title: "소울",
+    year: 2020,
+    category: "애니메이션, 가족",
+  },
+  {
+    id: 8,
+    title: "모아나",
+    year: 2016,
+    category: "애니메이션, 모험",
+  },
+  {
+    id: 9,
+    title: "기생충",
+    year: 2019,
+    category: "드라마, 스릴러",
+  },
+  {
+    id: 10,
+    title: "매트릭스",
+    year: 1999,
+    category: "액션, SF",
+  },
+];
